199fd6d51e45793d712533d4d1b589ba689a1d75
ramtin <ramtin_pourshakour@yahoo.com>
2023-10-04 14:56:22 +0200

    Initial commit

---
+++ src/test/java/com/example/ISBN_Validator/IsbnValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ISBN_Validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsbnValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
